c5124bc7de032ea015e1cd5a43bf2a2caec0b7ed
juzkiddin <88613134+juzkiddin@users.noreply.github.com>
2022-02-21 12:15:33 +0530

    Add files via upload

---
+++ Test.ino
@@ -0,0 +1,74 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+
+TinyGPSPlus gps;  // The TinyGPS++ object
+
+SoftwareSerial ss(4, 5); // The serial connection to the GPS device
+
+const char* ssid = "Home";
+const char* password = "sportback";
+
+float latitude , longitude, alti;
+String alt_str, lat_str , lng_str, final_data;
+
+WiFiServer server(80);
+void setup()
+{
+  Serial.begin(115200);
+  ss.begin(9600);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+
+}
+
+void loop()
+{
+
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+    {
+      if (gps.location.isValid())
+      {
+        latitude = gps.location.lat();
+        lat_str = String(latitude , 6);
+        longitude = gps.location.lng();
+        lng_str = String(longitude , 6);
+        alti = gps.altitude.meters();
+        alt_str = String(alti);
+        final_data = lat_str+" "+lng_str+" "+alt_str;
+      }
+    }
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client)
+  {
+    return;
+  }
+
+  String s;
+  //HTTP Protocol code.
+  s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+  s += final_data; //Our final raw data to return
+  
+  client.print(s);
+  delay(100);
+
+}
---
+++ test.py
@@ -0,0 +1,3 @@
+import nodemcu as nm
+while True:
+    print(nm.ret_data(input("Enter Query\n")))
